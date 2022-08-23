9428f3da1d149a4d18e858923a6dd2e4aa511cd5
Denis Makarov <qamakarov@gmail.com>
2022-08-23 12:23:56 +0200

    edit and update, new view for booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def edit
+    @duck = Duck.find(params[:id])
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to duck_path(@booking.duck)
+  end
 end
