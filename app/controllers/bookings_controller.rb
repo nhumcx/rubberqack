5643c05328000ea829c088375af6362c11edd316
NhuMo Dang <nhumo@careloop.io>
2022-08-23 13:02:06 +0200

    error fixed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -33,9 +33,10 @@ class BookingsController < ApplicationController
     @booking.update(booking_params)
     redirect_to duck_path(@booking.duck)
   end
-  
+
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
