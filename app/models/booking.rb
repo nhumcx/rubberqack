badb9e28c518abb4cd760d15e95394fc8a014c65
NhuMo Dang <nhumo@careloop.io>
2022-08-23 12:22:37 +0200

    new, create, delete, migrated status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user, dependent: :destroy
   belongs_to :duck, dependent: :destroy
+  validates :start_date, :end_date, presence: true
+  validates :status, inclusion: { in: %w[pending confirmed rejected] }
 end
