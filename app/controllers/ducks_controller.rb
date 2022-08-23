62eb5d6ebfdf43194062149e32245c78a1cbb2fa
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-08-23 11:37:04 +0200

    AddNameToDucks

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,2 +1,23 @@
 class DucksController < ApplicationController
+  def index
+    @ducks = Duck.all
+  end
+
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+  end
+
+  def create
+    @duck = Duck.new(duck_pramas)
+  end
+
+  private
+
+  def duck_pramas
+    params.require(:duck).permit(:category, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-    resources :ducks, only: %i[index show new creat] do
+    resources :ducks, only: %i[index show new create] do
       resources :bookings, only: %i[new create edit update]
     end
     resources :ducks, only: :destroy
