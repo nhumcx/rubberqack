0d06579757de9480b642807c835472e6b76141de
NhuMo Dang <nhumo@careloop.io>
2022-08-25 11:32:22 +0200

    trying w Emma

--- app/models/duck.rb
+++ app/models/duck.rb
@@ -2,4 +2,9 @@ class Duck < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_many :bookings
   has_one_attached :photo
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      {from }
+  end
 end
