47ee3c76b091858c9ffe1770c6d7c702292b8c6f
NhuMo Dang <nhumo@careloop.io>
2022-08-26 14:25:28 +0200

    finally managed the buttons

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "my_ducks", to: "pages#my_ducks"
 
   post "confirm", to: "bookings#confirm_booking"
-  post "confirm", to: "bookings#reject_booking"
+  post "reject", to: "bookings#reject_booking"
 
   resources :ducks, only: %i[index new create destroy show] do
     resources :bookings, only: %i[index new create edit update destroy]
